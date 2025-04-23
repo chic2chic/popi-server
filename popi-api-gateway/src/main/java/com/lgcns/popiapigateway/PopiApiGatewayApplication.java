637297652a10d0100f77556b6b00a6a275bd2390
woogieon8on <swp0927@naver.com>
2025-04-23 15:55:50 +0900

    [LCR-36] chore: api gateway 모듈 생성

---
+++ popi-api-gateway/src/main/java/com/lgcns/popiapigateway/PopiApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.popiapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopiApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopiApiGatewayApplication.class, args);
+    }
+
+}
---
+++ popi-api-gateway/src/test/java/com/lgcns/popiapigateway/PopiApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.popiapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopiApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
