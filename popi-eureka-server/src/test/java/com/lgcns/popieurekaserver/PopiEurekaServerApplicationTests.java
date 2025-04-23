a31af00d78f8ed606bb8796ce6e71a44015e8bb4
현태 최 <ht3064@naver.com>
2025-04-23 10:52:32 +0900

    [LCR-10] feat: eureka server 구성

---
+++ popi-eureka-server/src/test/java/com/lgcns/popieurekaserver/PopiEurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lgcns.popieurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopiEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
