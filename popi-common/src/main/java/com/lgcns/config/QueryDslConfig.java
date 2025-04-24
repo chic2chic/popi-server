61e44b37b7693824b7209ca101a52fdfe607b626
조영상 <51320595+0ssang@users.noreply.github.com>
2025-04-24 23:00:07 +0900

    [LCR-34] chore: 자동 주석 제거

--- popi-common/src/main/java/com/lgcns/config/QueryDslConfig.java
+++ popi-common/src/main/java/com/lgcns/config/QueryDslConfig.java
@@ -6,17 +6,6 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * packageName    : com.lgcns.config
- * fileName       : QueryDslConfig
- * author         : 조영상
- * date           : 4/23/25
- * description    : 자동 주석 생성
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 4/23/25         조영상        최초 생성
- */
 @Configuration
 public class QueryDslConfig {
     @PersistenceContext
