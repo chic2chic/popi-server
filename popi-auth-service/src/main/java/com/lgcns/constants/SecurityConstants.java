95755e4e2a0ab22b4d7af89605759538cc04dbbd
현태 최 <ht3064@naver.com>
2025-04-25 22:40:25 +0900

    [LCR-42] feat: 구글, 카카오 인증 서버로부터 ID 토큰 발급 기능 구현

---
+++ popi-auth-service/src/main/java/com/lgcns/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.lgcns.constants;
+
+public final class SecurityConstants {
+    public static final String KAKAO_LOGIN_URL = "https://kauth.kakao.com";
+    public static final String KAKAO_LOGIN_ENDPOINT = "/oauth/token";
+
+    public static final String GOOGLE_LOGIN_URL = "https://oauth2.googleapis.com";
+    public static final String GOOGLE_LOGIN_ENDPOINT = "/token";
+}
