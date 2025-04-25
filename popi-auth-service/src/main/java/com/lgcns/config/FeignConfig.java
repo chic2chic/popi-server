f23e5729479250baa31dc865744ea08bbc091ae8
현태 최 <ht3064@naver.com>
2025-04-25 22:31:43 +0900

    [LCR-42] chore: feign config 추가

---
+++ popi-auth-service/src/main/java/com/lgcns/config/FeignConfig.java
@@ -0,0 +1,18 @@
+package com.lgcns.config;
+
+import feign.RequestInterceptor;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.lgcns.client")
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate ->
+                requestTemplate.header(
+                        "Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+    }
+}
