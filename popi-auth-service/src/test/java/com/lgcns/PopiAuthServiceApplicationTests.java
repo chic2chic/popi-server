a34b68ee816d631ca2d9b72399fa8b8dac6e2f3d
현태 최 <ht3064@naver.com>
2025-04-25 21:09:12 +0900

    [LCR-28] chore: auth service module 생성

---
+++ popi-auth-service/src/test/java/com/lgcns/PopiAuthServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lgcns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopiAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
