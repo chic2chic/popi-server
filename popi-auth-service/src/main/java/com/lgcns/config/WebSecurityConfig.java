ceb05e9ae7c91ab9250fb4fac6e4b443f7b4ca0c
현태 최 <ht3064@naver.com>
2025-04-25 23:54:19 +0900

    [LCR-52] feat: security 설정

---
+++ popi-auth-service/src/main/java/com/lgcns/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.lgcns.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests(
+                auth -> auth.requestMatchers("/**").permitAll().anyRequest().authenticated());
+
+        return http.build();
+    }
+}
