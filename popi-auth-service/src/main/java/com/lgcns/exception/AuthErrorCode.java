694421b8b1982d5b9f38df18944ec075216b52af
현태 최 <ht3064@naver.com>
2025-04-26 11:28:08 +0900

    [LCR-42] feat: ID 토큰 검증 로직 구현

---
+++ popi-auth-service/src/main/java/com/lgcns/exception/AuthErrorCode.java
@@ -0,0 +1,21 @@
+package com.lgcns.exception;
+
+import com.lgcns.error.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+    ID_TOKEN_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "ID 토큰 검증에 실패했습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getErrorName() {
+        return this.name();
+    }
+}
---
+++ popi-auth-service/src/main/java/com/lgcns/service/IdTokenVerifier.java
@@ -0,0 +1,80 @@
+package com.lgcns.service;
+
+import com.lgcns.domain.OauthProvider;
+import com.lgcns.error.exception.CustomException;
+import com.lgcns.exception.AuthErrorCode;
+import java.time.Instant;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class IdTokenVerifier {
+
+    @Value("${oauth.google.client-id}")
+    private String googleClientId;
+
+    @Value("${oauth.kakao.client-id}")
+    private String kakaoClientId;
+
+    private final Map<OauthProvider, JwtDecoder> decoders =
+            Map.of(
+                    OauthProvider.GOOGLE, buildDecoder(OauthProvider.GOOGLE.getJwkSetUrl()),
+                    OauthProvider.KAKAO, buildDecoder(OauthProvider.KAKAO.getJwkSetUrl()));
+
+    private JwtDecoder buildDecoder(String jwkSetUrl) {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUrl).build();
+    }
+
+    public OidcUser getOidcUser(String idToken, OauthProvider provider) {
+        Jwt jwt = getJwt(idToken, provider);
+        OidcIdToken oidcIdToken = getOidcIdToken(jwt);
+
+        validateAudience(oidcIdToken, provider.getClientId(googleClientId, kakaoClientId));
+        validateIssuer(oidcIdToken, provider.getIssuer());
+        validateExpiresAt(oidcIdToken);
+
+        return new DefaultOidcUser(null, oidcIdToken);
+    }
+
+    private Jwt getJwt(String idToken, OauthProvider provider) {
+        return decoders.get(provider).decode(idToken);
+    }
+
+    private OidcIdToken getOidcIdToken(Jwt jwt) {
+        return new OidcIdToken(
+                jwt.getTokenValue(), jwt.getIssuedAt(), jwt.getExpiresAt(), jwt.getClaims());
+    }
+
+    private void validateAudience(OidcIdToken oidcIdToken, String clientId) {
+        String idTokenAudience = oidcIdToken.getAudience().get(0);
+
+        if (idTokenAudience == null || !idTokenAudience.equals(clientId)) {
+            throw new CustomException(AuthErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+
+    private void validateIssuer(OidcIdToken oidcIdToken, String issuer) {
+        String idTokenIssuer = oidcIdToken.getIssuer().toString();
+
+        if (idTokenIssuer == null || !idTokenIssuer.equals(issuer)) {
+            throw new CustomException(AuthErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+
+    private void validateExpiresAt(OidcIdToken oidcIdToken) {
+        Instant expiresAt = oidcIdToken.getExpiresAt();
+
+        if (expiresAt == null || expiresAt.isBefore(Instant.now())) {
+            throw new CustomException(AuthErrorCode.ID_TOKEN_VERIFICATION_FAILED);
+        }
+    }
+}
