7453b56b2d200701bfad97fbbcd91312c2d224ee
현태 최 <ht3064@naver.com>
2025-04-26 11:10:22 +0900

    [LCR-42] feat: jwt 유틸리티 구현

--- popi-auth-service/src/main/java/com/lgcns/constants/SecurityConstants.java
+++ popi-auth-service/src/main/java/com/lgcns/constants/SecurityConstants.java
@@ -1,6 +1,8 @@
 package com.lgcns.constants;
 
 public final class SecurityConstants {
+    public static final String TOKEN_ROLE_NAME = "role";
+
     public static final String KAKAO_LOGIN_URL = "https://kauth.kakao.com";
     public static final String KAKAO_LOGIN_ENDPOINT = "/oauth/token";
 
