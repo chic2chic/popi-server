8fab7bea94f645d54336fa46644dcb217d2a804c
현태 최 <ht3064@naver.com>
2025-05-02 03:16:14 +0900

    [LCR-78] refactor: 인증 서비스 SwaggerConfig에서 불필요한 accessToken 보안 설정 제거

--- popi-auth-service/src/main/java/com/lgcns/config/SwaggerConfig.java
+++ popi-auth-service/src/main/java/com/lgcns/config/SwaggerConfig.java
@@ -1,10 +1,7 @@
 package com.lgcns.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,27 +16,9 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(
                         new Info()
-                                .title("PoPI Auth Server API")
+                                .title("PoPI Auth Service API")
                                 .description("PoPI 인증 서비스 API 명세서입니다.")
                                 .version("v0.0.1"))
-                .addServersItem(new Server().url("/auth"))
-                .components(authSetting())
-                .addSecurityItem(securityRequirement());
-    }
-
-    private Components authSetting() {
-        return new Components()
-                .addSecuritySchemes(
-                        "accessToken",
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .in(SecurityScheme.In.HEADER)
-                                .name("Authorization"));
-    }
-
-    private SecurityRequirement securityRequirement() {
-        return new SecurityRequirement().addList("accessToken");
+                .addServersItem(new Server().url("/auth"));
     }
 }
