36cf2e574315728f2ab4f52ccac8c89fe82eb5f0
현태 최 <ht3064@naver.com>
2025-05-02 17:35:30 +0900

    [LCR-89] refactor: 리프레시 토큰을 쿠키로 전달하도록 소셜 로그인 응답 방식 수정

--- popi-auth-service/src/main/java/com/lgcns/controller/AuthController.java
+++ popi-auth-service/src/main/java/com/lgcns/controller/AuthController.java
@@ -4,9 +4,12 @@ import com.lgcns.domain.OauthProvider;
 import com.lgcns.dto.request.AuthCodeRequest;
 import com.lgcns.dto.response.SocialLoginResponse;
 import com.lgcns.service.AuthService;
+import com.lgcns.util.CookieUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "인증 서버 API", description = "인증 서버 API입니다.")
@@ -15,12 +18,18 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieUtil cookieUtil;
 
     @Operation(summary = "회원가입 및 로그인", description = "회원가입 및 로그인을 진행합니다.")
     @PostMapping("/social-login")
-    public SocialLoginResponse memberSocialLogin(
+    public ResponseEntity<SocialLoginResponse> memberSocialLogin(
             @RequestParam(name = "oauthProvider") OauthProvider provider,
             @RequestBody AuthCodeRequest request) {
-        return authService.socialLoginMember(request, provider);
+        SocialLoginResponse response = authService.socialLoginMember(request, provider);
+
+        String refreshToken = response.refreshToken();
+        HttpHeaders headers = cookieUtil.generateRefreshTokenCookie(refreshToken);
+
+        return ResponseEntity.ok().headers(headers).body(response);
     }
 }
