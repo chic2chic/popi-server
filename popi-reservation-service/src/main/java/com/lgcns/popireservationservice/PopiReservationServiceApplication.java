33f5e1ed1521aef5da38e8e002f027f47f47e935
jeongyeon0208 <githubb0208@gmail.com>
2025-05-16 12:23:34 +0900

    chore: 예약 마이크로서비스 초기 생성

---
+++ popi-reservation-service/src/main/java/com/lgcns/popireservationservice/PopiReservationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lgcns.popireservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopiReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopiReservationServiceApplication.class, args);
+    }
+}
---
+++ popi-reservation-service/src/test/java/com/lgcns/popireservationservice/PopiReservationServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lgcns.popireservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopiReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
