7b8d82e58a265372e4382c104543422387b17d2a
woogieon8on <swp0927@naver.com>
2025-05-20 10:57:13 +0900

    [LCR-162] chore: 알림 마이크로서비스 초기 생성

---
+++ popi-notification-service/src/main/java/com/lgcns/PopiNotificationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lgcns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopiNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopiNotificationServiceApplication.class, args);
+    }
+}
