61a0c43799c7de5f2ed58f00a6bc1399b4ee3226
Youngsang Cho <51320595+0ssang@users.noreply.github.com>
2025-05-21 09:58:10 +0900

    chore: 팝업 마이크로서비스 초기 세팅 (#31)
    * chore: 팝업 마이크로서비스 초기 생성
    
    * chore: application-local.yml 초기 설정 추가
    
    * chore: Swagger 설정
    
    * chore: API Gateway에 팝업 서비스 라우팅 및 Swagger config 파일 변경
    
    * chore: 팝업 서비스 Dockerfile 추가
    
    * [LCR-174] chore: DB 의존성 추가
    
    * [LCR-174] chore: build.gradle 의존성 추가
    
    * [LCR-174] fix: 팝업 서비스 인증 필터 제거
    
    * [LCR-174] fix: DB 의존성 제거
    
    * [LCR-174] chore: application-local.yml 파일 수정
    
    * [LCR-174] fix: Swagger 설정 파일 수정

---
+++ popi-popup-service/src/main/java/com/lgcns/PopiPopupServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lgcns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopiPopupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopiPopupServiceApplication.class, args);
+    }
+}
