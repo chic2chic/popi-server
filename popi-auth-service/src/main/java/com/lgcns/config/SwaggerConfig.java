4f25b2e21ff2358555f3a845a2a73b60394f358b
현태 최 <ht3064@naver.com>
2025-05-21 12:23:06 +0900

    [LCR-165] feat: Swagger에 JWT 인증 설정 추가

--- popi-auth-service/src/main/java/com/lgcns/config/SwaggerConfig.java
+++ popi-auth-service/src/main/java/com/lgcns/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.lgcns.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +22,24 @@ public class SwaggerConfig {
                                 .title("PoPI Auth Service API")
                                 .description("PoPI 인증 서비스 API 명세서입니다.")
                                 .version("v0.0.1"))
-                .addServersItem(new Server().url("/auth"));
+                .addServersItem(new Server().url("/auth"))
+                .components(authSetting())
+                .addSecurityItem(securityRequirement());
+    }
+
+    private Components authSetting() {
+        return new Components()
+                .addSecuritySchemes(
+                        "accessToken",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization"));
+    }
+
+    private SecurityRequirement securityRequirement() {
+        return new SecurityRequirement().addList("accessToken");
     }
 }
