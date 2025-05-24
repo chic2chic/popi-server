63e23e29302bf595783de374c403c268936f805f
현태 최 <ht3064@naver.com>
2025-05-24 19:40:45 +0900

    [LCR-199] test: 회원 정보 조회 테스트 코드 수정

--- popi-member-service/src/test/java/com/lgcns/service/MemberServiceTest.java
+++ popi-member-service/src/test/java/com/lgcns/service/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package com.lgcns.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import com.lgcns.WireMockIntegrationTest;
 import com.lgcns.domain.Member;
@@ -10,8 +11,12 @@ import com.lgcns.enums.MemberAge;
 import com.lgcns.enums.MemberGender;
 import com.lgcns.enums.MemberRole;
 import com.lgcns.enums.MemberStatus;
+import com.lgcns.error.exception.CustomException;
+import com.lgcns.exception.MemberErrorCode;
 import com.lgcns.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,33 +25,43 @@ class MemberServiceTest extends WireMockIntegrationTest {
     @Autowired private MemberService memberService;
     @Autowired private MemberRepository memberRepository;
 
-    private Member registerAuthenticatedMember() {
-        Member member =
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        member =
                 Member.createMember(
                         OauthInfo.createOauthInfo("testOauthId", "testOauthProvider"),
                         "testNickname",
                         MemberGender.MALE,
                         MemberAge.TWENTIES);
         memberRepository.save(member);
-
-        return member;
     }
 
-    @Test
-    void 회원_정보를_조회한다() {
-        // given
-        registerAuthenticatedMember();
+    @Nested
+    class 회원_정보를_조회할_때 {
 
-        // when
-        MemberInfoResponse response = memberService.findMemberInfo("1");
+        @Test
+        void 회원이_존재하면_조회에_성공한다() {
+            // when
+            MemberInfoResponse response = memberService.findMemberInfo("1");
 
-        // then
-        Assertions.assertAll(
-                () -> assertThat(response.memberId()).isEqualTo(1L),
-                () -> assertThat(response.nickname()).isEqualTo("testNickname"),
-                () -> assertThat(response.role()).isEqualTo(MemberRole.USER),
-                () -> assertThat(response.gender()).isEqualTo(MemberGender.MALE),
-                () -> assertThat(response.age()).isEqualTo(MemberAge.TWENTIES),
-                () -> assertThat(response.status()).isEqualTo(MemberStatus.NORMAL));
+            // then
+            Assertions.assertAll(
+                    () -> assertThat(response.memberId()).isEqualTo(1L),
+                    () -> assertThat(response.nickname()).isEqualTo("testNickname"),
+                    () -> assertThat(response.age()).isEqualTo(MemberAge.TWENTIES),
+                    () -> assertThat(response.gender()).isEqualTo(MemberGender.MALE),
+                    () -> assertThat(response.status()).isEqualTo(MemberStatus.NORMAL),
+                    () -> assertThat(response.role()).isEqualTo(MemberRole.USER));
+        }
+
+        @Test
+        void 회원이_존재하지_않으면_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> memberService.findMemberInfo("999"))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(MemberErrorCode.MEMBER_NOT_FOUND.getMessage());
+        }
     }
 }
