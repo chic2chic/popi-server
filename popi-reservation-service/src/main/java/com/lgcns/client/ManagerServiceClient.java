892862604b7c989ad0b2db02a37b38b4f2bb384f
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-05-25 12:31:41 +0900

    [LCR-168] feat: 내부 호출을 위한 FeignClient 함수 정의

--- popi-reservation-service/src/main/java/com/lgcns/client/ManagerServiceClient.java
+++ popi-reservation-service/src/main/java/com/lgcns/client/ManagerServiceClient.java
@@ -2,17 +2,22 @@ package com.lgcns.client;
 
 import com.lgcns.client.dto.MonthlyReservationResponse;
 import com.lgcns.config.FeignConfig;
+import com.lgcns.dto.response.SurveyChoiceResponse;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
-        name = "popi-manager-service",
-        url = "${manager-service-url:}",
+        name = "${manager.service.name}",
+        url = "${manager.service.url:}",
         configuration = FeignConfig.class)
 public interface ManagerServiceClient {
     @GetMapping("/internal/reservations/popups/{popupId}")
     MonthlyReservationResponse findMonthlyReservation(
             @PathVariable Long popupId, @RequestParam String date);
+
+    @GetMapping("/internal/reservations/popups/{popupId}/survey")
+    List<SurveyChoiceResponse> findSurveyChoicesByPopupId(@PathVariable Long popupId);
 }
