ec91854588d90aedad4863e55527994456df68d0
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-26 17:13:58 +0900

    [LCR-185] feat: 상품 상세 조회 서비스 로직 구현

--- popi-popup-service/src/main/java/com/lgcns/service/PopupService.java
+++ popi-popup-service/src/main/java/com/lgcns/service/PopupService.java
@@ -1,9 +1,12 @@
 package com.lgcns.service;
 
+import com.lgcns.dto.response.PopupDetailsResponse;
 import com.lgcns.dto.response.PopupInfoResponse;
 import com.lgcns.response.SliceResponse;
 
 public interface PopupService {
     SliceResponse<PopupInfoResponse> findPopupsByName(
             String searchName, Long lastPopupId, int size);
+
+    PopupDetailsResponse findPopupDetailsById(Long popupId);
 }
--- popi-popup-service/src/main/java/com/lgcns/service/PopupServiceImpl.java
+++ popi-popup-service/src/main/java/com/lgcns/service/PopupServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lgcns.service;
 
 import com.lgcns.client.ManagerServiceClient;
+import com.lgcns.dto.response.PopupDetailsResponse;
 import com.lgcns.dto.response.PopupInfoResponse;
 import com.lgcns.response.SliceResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,5 +16,10 @@ public class PopupServiceImpl implements PopupService {
     public SliceResponse<PopupInfoResponse> findPopupsByName(
             String searchName, Long lastPopupId, int size) {
         return managerServiceClient.findPopupsByName(searchName, lastPopupId, size);
+    }
+
+    @Override
+    public PopupDetailsResponse findPopupDetailsById(Long popupId) {
+        return managerServiceClient.findPopupDetailsById(popupId);
     }
 }
