258b7a5425a32d7b4889a7d70efdf9c1f935b670
jeongyeon0208 <githubb0208@gmail.com>
2025-05-27 00:13:28 +0900

    feat: ManagerServiceClient에 예약 정보 조회 추가

--- popi-reservation-service/src/main/java/com/lgcns/client/managerClient/ManagerServiceClient.java
+++ popi-reservation-service/src/main/java/com/lgcns/client/managerClient/ManagerServiceClient.java
@@ -2,11 +2,15 @@ package com.lgcns.client.managerClient;
 
 import com.lgcns.client.managerClient.dto.request.PopupIdsRequest;
 import com.lgcns.client.managerClient.dto.response.MonthlyReservationResponse;
+import com.lgcns.client.managerClient.dto.response.ReservationInfoResponse;
 import com.lgcns.client.managerClient.dto.response.ReservationPopupInfoResponse;
 import com.lgcns.config.FeignConfig;
 import com.lgcns.dto.response.SurveyChoiceResponse;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(
@@ -23,4 +27,6 @@ public interface ManagerServiceClient {
 
     @PostMapping("/internal/reservations")
     List<ReservationPopupInfoResponse> findReservedPopupInfo(@RequestBody PopupIdsRequest request);
+    @GetMapping("/internal/reservations/{reservationId}")
+    ReservationInfoResponse reservationFindById(@PathVariable("reservationId") Long reservationId);
 }
