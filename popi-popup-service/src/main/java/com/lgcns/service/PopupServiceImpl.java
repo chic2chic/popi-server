6c0b32ef5516d5fb1dbdf66ff92718a0358f83b6
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-26 23:18:28 +0900

    [LCR-185] style: DI 들여쓰기 적용

--- popi-popup-service/src/main/java/com/lgcns/service/PopupServiceImpl.java
+++ popi-popup-service/src/main/java/com/lgcns/service/PopupServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PopupServiceImpl implements PopupService {
+
     private final ManagerServiceClient managerServiceClient;
 
     @Override
