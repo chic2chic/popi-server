962d28f524900307c23d5e618a88a449ffb858fc
woogieon8on <swp0927@naver.com>
2025-05-26 10:20:05 +0900

    [LCR-220] chore: 상품 서비스 초기 세팅

---
+++ popi-item-service/src/main/java/com/lgcns/PopiItemServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lgcns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopiItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopiItemServiceApplication.class, args);
+    }
+}
