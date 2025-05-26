4f2666372c07798421e835eec3b828be662f74d2
jeongyeon0208 <githubb0208@gmail.com>
2025-05-27 00:19:18 +0900

    feat: 회원 예약 생성 api 구현

--- popi-reservation-service/src/main/java/com/lgcns/externalApi/MemberReservationController.java
+++ popi-reservation-service/src/main/java/com/lgcns/externalApi/MemberReservationController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,6 +33,14 @@ public class MemberReservationController {
     public List<SurveyChoiceResponse> choiceListByPopupIdFind(
             @RequestHeader("member-id") String memberId, @PathVariable Long popupId) {
         return memberReservationService.findSurveyChoicesByPopupId(memberId, popupId);
+    }
+
+    @PostMapping("/{reservationId}")
+    @Operation(summary = "회원 예약 생성", description = "예약을 생성합니다. 예약 ID를 사용하여 예약을 생성합니다.")
+    public ResponseEntity<Void> createMemberReservation(
+            @RequestHeader("member-id") String memberId, @PathVariable Long reservationId) {
+        memberReservationService.createMemberReservation(memberId, reservationId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
