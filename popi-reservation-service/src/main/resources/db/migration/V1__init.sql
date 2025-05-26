231ad30bbd3517c5a93ede8dce69ad9e472624e8
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-05-26 14:05:20 +0900

    chore: 예약 서비스 flyway 설정 (#51)
    * [LCR-218] chore: flyway 의존성 추가
    
    * [LCR-218] chore: member_reservation table init sql 파일 생성
    
    * [LCR-218] chore: application-local, application-test flyway 설정 추가
    
    * [LCR-218] chore: member_reservation 테이블 컬럼 추가
    
    * [LCR-218] refactor: imageByte에서 qrImage로 도메인 컬럼명 수정
    
    * [LCR-218] chore: flyway DB 컬럼명 및 TEXT로  타입 수정

---
+++ popi-reservation-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE member_reservation
+(
+    member_reservation_id BIGINT AUTO_INCREMENT NOT NULL,
+    reservation_id        BIGINT   NOT NULL,
+    member_id             BIGINT   NOT NULL,
+    popup_id              BIGINT NULL,
+    qr_image              TEXT NULL,
+    reservation_date      DATE,
+    reservation_time      TIME,
+    created_at            DATETIME NOT NULL,
+    updated_at            DATETIME
+);
