920c43d40025a54f600ae4d94f537db2eae2869d
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-05-26 10:39:04 +0900

    [LCR-218] chore: flyway DB 컬럼명 및 TEXT로  타입 수정

--- popi-reservation-service/src/main/resources/db/migration/V1__init.sql
+++ popi-reservation-service/src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE member_reservation
 (
     member_reservation_id BIGINT AUTO_INCREMENT NOT NULL,
-    reservation_id        BIGINT NOT NULL,
-    member_id             BIGINT NOT NULL,
+    reservation_id        BIGINT   NOT NULL,
+    member_id             BIGINT   NOT NULL,
     popup_id              BIGINT NULL,
-    image_byte            BLOB NULL,
+    qr_image              TEXT NULL,
     reservation_date      DATE,
     reservation_time      TIME,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME
+    created_at            DATETIME NOT NULL,
+    updated_at            DATETIME
 );
