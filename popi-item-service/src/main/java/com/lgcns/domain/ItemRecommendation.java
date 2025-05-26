37831ebc91c57e555e4f9161489ff1309fac2d16
woogieon8on <swp0927@naver.com>
2025-05-26 14:55:54 +0900

    [LCR-220] feat: ItemRecommendation 엔티티 생성

---
+++ popi-item-service/src/main/java/com/lgcns/domain/ItemRecommendation.java
@@ -0,0 +1,63 @@
+package com.lgcns.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemRecommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_recommendation_id")
+    private Long id;
+
+    private Long memberId;
+
+    private Long popupId;
+
+    private Long itemId;
+
+    private String itemName;
+
+    private int itemPrice;
+
+    private String itemImageUrl;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ItemRecommendation(
+            Long memberId,
+            Long popupId,
+            Long itemId,
+            String itemName,
+            int itemPrice,
+            String itemImageUrl) {
+        this.memberId = memberId;
+        this.popupId = popupId;
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemImageUrl = itemImageUrl;
+    }
+
+    public static ItemRecommendation createItemRecommendation(
+            Long memberId,
+            Long popupId,
+            Long itemId,
+            String itemName,
+            int itemPrice,
+            String itemImageUrl) {
+        return ItemRecommendation.builder()
+                .memberId(memberId)
+                .popupId(popupId)
+                .itemId(itemId)
+                .itemName(itemName)
+                .itemPrice(itemPrice)
+                .itemImageUrl(itemImageUrl)
+                .build();
+    }
+}
