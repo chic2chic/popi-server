a4492b7bb03e00b8d79b5baf60c29b19b269ca62
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-05-26 14:32:51 +0900

    [LCR-226] fix: member_reservation_id 컬럼의 누락된 PK 설정 추가

--- popi-reservation-service/src/main/resources/db/migration/V1__init.sql
+++ popi-reservation-service/src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE member_reservation
 (
-    member_reservation_id BIGINT AUTO_INCREMENT NOT NULL,
+    member_reservation_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     reservation_id        BIGINT   NOT NULL,
     member_id             BIGINT   NOT NULL,
     popup_id              BIGINT NULL,
