16b0deb01bb97326f40c68ace1f31d6cb59ca6e8
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-27 15:35:40 +0900

    [LCR-223] feat: 인기 팝업 Id를 조회하는 내부 api 구현

---
+++ popi-reservation-service/src/main/java/com/lgcns/internalApi/MemberReservationInternalController.java
@@ -0,0 +1,21 @@
+package com.lgcns.internalApi;
+
+import com.lgcns.service.MemberReservationService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal")
+public class MemberReservationInternalController {
+
+    private final MemberReservationService memberReservationService;
+
+    @GetMapping("/popups/hot")
+    public List<Long> findHotPopups() {
+        return memberReservationService.findHotPopupIds();
+    }
+}
