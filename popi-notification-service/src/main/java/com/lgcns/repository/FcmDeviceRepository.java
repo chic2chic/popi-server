9e39e1c9b51e20c44dc9c0a157229678cc5656fc
woogieon8on <swp0927@naver.com>
2025-05-27 13:26:05 +0900

    [LCR-159] refactor: FcmDevice 엔티티명 수정

--- popi-notification-service/src/main/java/com/lgcns/repository/FcmDeviceRepository.java
+++ popi-notification-service/src/main/java/com/lgcns/repository/FcmDeviceRepository.java
@@ -3,4 +3,5 @@ package com.lgcns.repository;
 import com.lgcns.domain.FcmDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FcmTokenRepository extends JpaRepository<FcmDevice, Long> {}
+public interface FcmDeviceRepository
+        extends JpaRepository<FcmDevice, Long>, FcmDeviceRepositoryCustom {}
