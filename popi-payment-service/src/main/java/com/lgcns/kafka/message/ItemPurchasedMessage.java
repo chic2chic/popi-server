8abffc5364874087b97c86c7ea978dfa842862d8
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-05-31 09:54:46 +0900

    feat: Kafka 메시지 purchasedAt 필드 문자열 포맷으로 변경 (#74)

--- popi-payment-service/src/main/java/com/lgcns/kafka/message/ItemPurchasedMessage.java
+++ popi-payment-service/src/main/java/com/lgcns/kafka/message/ItemPurchasedMessage.java
@@ -1,11 +1,16 @@
 package com.lgcns.kafka.message;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lgcns.domain.Payment;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record ItemPurchasedMessage(
-        Long popupId, List<Item> items, int amount, LocalDateTime purchasedAt) {
+        Long popupId,
+        List<Item> items,
+        int amount,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+                LocalDateTime purchasedAt) {
     public static ItemPurchasedMessage from(Payment payment) {
         List<Item> items =
                 payment.getItems().stream()
