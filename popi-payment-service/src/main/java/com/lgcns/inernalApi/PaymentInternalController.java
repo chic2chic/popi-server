e065c03a760e0ed319cfc2b02d96d86d2f705c26
현태 최 <ht3064@naver.com>
2025-06-02 02:42:54 +0900

    [LCR-290] feat: 상품별 구매자 수 조회 API에 Swagger 문서 설명 추가

--- popi-payment-service/src/main/java/com/lgcns/inernalApi/PaymentInternalController.java
+++ popi-payment-service/src/main/java/com/lgcns/inernalApi/PaymentInternalController.java
@@ -2,6 +2,7 @@ package com.lgcns.inernalApi;
 
 import com.lgcns.dto.response.ItemBuyerCountResponse;
 import com.lgcns.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ public class PaymentInternalController {
     private final PaymentService paymentService;
 
     @GetMapping("/{popupId}/buyer-counts")
+    @Operation(summary = "상품별 구매자 수 조회", description = "해당 팝업에서 상품별로 중복되지 않는 구매자 수를 조회합니다.")
     public List<ItemBuyerCountResponse> countItemBuyerByPopupId(
             @PathVariable(name = "popupId") Long popupId) {
         return paymentService.countItemBuyerByPopupId(popupId);
