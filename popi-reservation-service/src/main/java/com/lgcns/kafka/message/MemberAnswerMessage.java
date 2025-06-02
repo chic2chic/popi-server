075bc759e815686f152607d696ad2bdfda99eccf
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-06-02 21:56:34 +0900

    [LCR-181] feat: 요청 DTO인 Request와 Kafka 통신을 위한 DTO의 관심사 분리

--- popi-reservation-service/src/main/java/com/lgcns/kafka/message/MemberAnswerMessage.java
+++ popi-reservation-service/src/main/java/com/lgcns/kafka/message/MemberAnswerMessage.java
@@ -1,10 +1,13 @@
 package com.lgcns.kafka.message;
 
 import com.lgcns.dto.request.SurveyChoiceRequest;
+import com.lgcns.kafka.dto.SurveyChoiceDto;
 import java.util.List;
 
-public record MemberAnswerMessage(Long memberId, List<SurveyChoiceRequest> surveyChoices) {
-    public static MemberAnswerMessage of(Long memberId, List<SurveyChoiceRequest> surveyChoices) {
+public record MemberAnswerMessage(Long memberId, List<SurveyChoiceDto> surveyChoices) {
+    public static MemberAnswerMessage of(Long memberId, List<SurveyChoiceRequest> requests) {
+        List<SurveyChoiceDto> surveyChoices =
+                requests.stream().map(r -> SurveyChoiceDto.of(r.surveyId(), r.choiceId())).toList();
         return new MemberAnswerMessage(memberId, surveyChoices);
     }
 }
