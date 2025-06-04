20dab5747b06cfb586b10f4c205639902f5ec35d
woogieon8on <swp0927@naver.com>
2025-06-04 13:20:15 +0900

    [LCR-159] feat: Redis 설정 추가

--- popi-notification-service/src/main/java/com/lgcns/infra/properties/PropertiesConfig.java
+++ popi-notification-service/src/main/java/com/lgcns/infra/properties/PropertiesConfig.java
@@ -1,9 +1,10 @@
 package com.lgcns.infra.properties;
 
 import com.lgcns.infra.firebase.FirebaseProperties;
+import com.lgcns.infra.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({FirebaseProperties.class})
+@EnableConfigurationProperties({FirebaseProperties.class, RedisProperties.class})
 @Configuration
 public class PropertiesConfig {}
---
+++ popi-notification-service/src/main/java/com/lgcns/infra/redis/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.lgcns.infra.redis;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig =
+                new RedisStandaloneConfiguration(redisProperties.host(), redisProperties.port());
+
+        redisStandaloneConfig.setDatabase(redisProperties.database());
+
+        if (!redisProperties.password().isBlank()) {
+            redisStandaloneConfig.setPassword(redisProperties.password());
+        }
+
+        LettuceClientConfiguration lettuceClientConfig =
+                LettuceClientConfiguration.builder()
+                        .commandTimeout(Duration.ofSeconds(1))
+                        .shutdownTimeout(Duration.ZERO)
+                        .build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfig, lettuceClientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Long.class));
+        return template;
+    }
+}
