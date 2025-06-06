fbb17d29c68e4e19fc303c0fea83df9442c74520
현태 최 <ht3064@naver.com>
2025-06-06 18:35:52 +0900

    [LCR-159] chore: FirebaseProperties 삭제 및 @Value 주입 방식으로 변경

--- popi-notification-service/src/main/java/com/lgcns/infra/firebase/FirebaseProperties.java
+++
@@ -1,6 +0,0 @@
-package com.lgcns.infra.firebase;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "firebase")
-public record FirebaseProperties(String credentialsJson) {}
--- popi-notification-service/src/main/java/com/lgcns/infra/properties/PropertiesConfig.java
+++ popi-notification-service/src/main/java/com/lgcns/infra/properties/PropertiesConfig.java
@@ -1,10 +1,9 @@
 package com.lgcns.infra.properties;
 
-import com.lgcns.infra.firebase.FirebaseProperties;
 import com.lgcns.infra.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({FirebaseProperties.class, RedisProperties.class})
+@EnableConfigurationProperties({RedisProperties.class})
 @Configuration
 public class PropertiesConfig {}
