b96b106302ed2be175b49f0cf21e465a3ef91887
조영상 <51320595+0ssang@users.noreply.github.com>
2025-06-09 03:24:45 +0900

    [LCR-320] chore: 디렉토리 변경

--- popi-popup-service/src/test/java/com/lgcns/service/integration/PopupServiceIntegrationTest.java
+++ popi-popup-service/src/test/java/com/lgcns/service/integration/PopupServiceIntegrationTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lgcns.WireMockIntegrationTest;
 import com.lgcns.client.managerClient.dto.PopupIdsRequest;
 import com.lgcns.dto.response.PopupDetailsResponse;
 import com.lgcns.dto.response.PopupInfoResponse;
