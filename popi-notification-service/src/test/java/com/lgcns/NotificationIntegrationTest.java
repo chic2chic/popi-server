86d2ba3c09f8639f28706929084ac2b6ccf88d03
woogieon8on <swp0927@naver.com>
2025-06-11 13:11:56 +0900

    [LCR-332] refactor: 테스트 코드에서 불필요한 Mock 처리 제거

--- popi-notification-service/src/test/java/com/lgcns/NotificationIntegrationTest.java
+++ popi-notification-service/src/test/java/com/lgcns/NotificationIntegrationTest.java
@@ -1,8 +1,5 @@
 package com.lgcns;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.lgcns.repository.FcmDeviceRepository;
 import com.lgcns.service.FcmService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,7 +9,4 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @ActiveProfiles("test")
 public class NotificationIntegrationTest {
     @MockitoBean protected FcmService fcmService;
-    @MockitoBean protected FirebaseMessaging firebaseMessaging;
-    @MockitoBean protected FirebaseApp firebaseApp;
-    @MockitoBean protected FcmDeviceRepository fcmDeviceRepository;
 }
