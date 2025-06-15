2b6fe8d0e96b12862046cd56cc1f67f0d3c74065
현태 최 <ht3064@naver.com>
2025-06-15 21:16:21 +0900

    [LCR-364] chore: GrpcClientConfig에 test profile 분리 적용하여 테스트 환경 제외

--- popi-member-service/src/main/java/com/lgcns/config/GrpcClientConfig.java
+++ popi-member-service/src/main/java/com/lgcns/config/GrpcClientConfig.java
@@ -5,8 +5,10 @@ import io.grpc.Channel;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class GrpcClientConfig {
 
     @GrpcClient("auth-service")
