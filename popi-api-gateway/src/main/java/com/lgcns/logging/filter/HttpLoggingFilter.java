46b095a8d81aa4868f883f4115525c21a1ec30db
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-06-15 17:43:33 +0900

    [LCR-344] refactor: gateway에서 uri, traceId 넘기도록 수정

--- popi-api-gateway/src/main/java/com/lgcns/logging/filter/HttpLoggingFilter.java
+++ popi-api-gateway/src/main/java/com/lgcns/logging/filter/HttpLoggingFilter.java
@@ -3,6 +3,7 @@ package com.lgcns.logging.filter;
 import com.lgcns.logging.dto.HttpRequestLogInfo;
 import com.lgcns.logging.dto.HttpResponseLogInfo;
 import java.util.UUID;
+import java.util.regex.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.slf4j.MDC;
@@ -26,15 +27,25 @@ public class HttpLoggingFilter implements GlobalFilter {
 
     private static final DataBufferFactory bufferFactory = new DefaultDataBufferFactory();
 
+    private static final Pattern EXCLUDED_PATH_PATTERN =
+            Pattern.compile("^/[^/]+/(v3/api-docs|actuator|prometheus)(/.*)?$");
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (EXCLUDED_PATH_PATTERN.matcher(path).matches()) {
+            return chain.filter(exchange);
+        }
+
         String traceId = UUID.randomUUID().toString();
         MDC.put("traceId", traceId);
 
-        logRequest(exchange);
+        ServerHttpRequest mutatedRequest =
+                exchange.getRequest().mutate().header("trace-id", traceId).build();
 
-        ServerHttpRequest request = exchange.getRequest();
-        String path = request.getPath().value();
+        logRequest(mutatedRequest);
+
         ServerHttpResponse originalResponse = exchange.getResponse();
 
         // 응답 바디를 가로채기 위해 응답 객체를 데코레이터로 감쌈
@@ -75,12 +86,15 @@ public class HttpLoggingFilter implements GlobalFilter {
                 };
 
         // 응답 객체만 데코레이터로 변환 + 체인 필터 실행
-        return chain.filter(exchange.mutate().response(decoratedResponse).build())
+        return chain.filter(
+                        exchange.mutate()
+                                .request(mutatedRequest)
+                                .response(decoratedResponse)
+                                .build())
                 .doFinally(signal -> MDC.clear());
     }
 
-    private static void logRequest(ServerWebExchange exchange) {
-        ServerHttpRequest request = exchange.getRequest();
+    private static void logRequest(ServerHttpRequest request) {
         log.info("{}", HttpRequestLogInfo.from(request));
     }
 
