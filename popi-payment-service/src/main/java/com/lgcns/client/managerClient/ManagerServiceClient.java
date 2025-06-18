a2eb1777c93aa6ff22956b8f9a2d903eab82a3b8
현태 최 <ht3064@naver.com>
2025-06-18 10:14:20 +0900

    [LCR-371] refactor: FeignClient name 속성에 기본값 설정 추가

--- popi-payment-service/src/main/java/com/lgcns/client/managerClient/ManagerServiceClient.java
+++ popi-payment-service/src/main/java/com/lgcns/client/managerClient/ManagerServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-        name = "${manager.service.name}",
+        name = "${manager.service.name:}",
         url = "${manager.service.url:}",
         configuration = FeignConfig.class)
 public interface ManagerServiceClient {
