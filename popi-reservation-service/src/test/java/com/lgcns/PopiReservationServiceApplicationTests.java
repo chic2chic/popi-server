a654f142ede64155e2a714a7f7b7ccadc38530a1
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-06-18 21:01:50 +0900

    [LCR-372] test: gRPC 통합 테스트 위한 테스트 서버 및 Config 작성

--- popi-reservation-service/src/test/java/com/lgcns/PopiReservationServiceApplicationTests.java
+++ popi-reservation-service/src/test/java/com/lgcns/PopiReservationServiceApplicationTests.java
@@ -1,11 +1,14 @@
 package com.lgcns;
 
+import com.lgcns.service.integration.GrpcClientTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(GrpcClientTestConfig.class)
 class PopiReservationServiceApplicationTests {
 
     @Test
---
+++ popi-reservation-service/src/test/java/com/lgcns/service/integration/IntegrationTest.java
@@ -0,0 +1,36 @@
+package com.lgcns.service.integration;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import java.io.IOException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureWireMock(port = 0)
+@Import(GrpcClientTestConfig.class)
+public abstract class IntegrationTest {
+
+    @Autowired protected WireMockServer wireMockServer;
+    @Autowired private InMemoryGrpcServer inMemoryGrpcServer;
+
+    @BeforeEach
+    void restartWireMock() throws IOException {
+        wireMockServer.stop();
+        wireMockServer.start();
+
+        inMemoryGrpcServer.start(new TestMemberGrpcService());
+    }
+
+    @AfterEach
+    void resetWireMock() {
+        wireMockServer.resetAll();
+
+        inMemoryGrpcServer.shutdown();
+    }
+}
